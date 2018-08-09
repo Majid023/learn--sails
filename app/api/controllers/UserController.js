53b4ebd1f2d3c5207367b9f5287aa1a26c940a9e
Majid Ansari <33059093+Majid023@users.noreply.github.com>
2018-08-09 11:39:16 +0530

    add session polices

--- app/api/controllers/UserController.js
+++ app/api/controllers/UserController.js
@@ -43,6 +43,8 @@ module.exports = {
     },
     index: function(req, res, next) {
         
+        // console.log(new Date());
+        // console.log(req.session.authenticated);
         User.find( function foundUsers (err, users) {
             if(err) return next(err);
 
